1dfb67a6acbe524f750d2504cef2ff8ff7ddb570
Domovski1 <624273242@mail.ru>
2021-03-04 08:24:28 +0300

    Работа с ворд, сэсви, эксель

---
+++ LearnWord/Classes/LevenshteinClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWord.Classes
+{
+    public class LevenshteinClass
+    {
+        public static int Minimum(int a, int b, int c) => (a = a < b ? a : b) < c ? a : c;
+
+
+
+        /// <summary>
+        /// Принимает два слова и возврашает число операций, необходимых совершить над первым словом, чтобы  получилось второе
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static int Distance (string first, string second)
+        {
+            int n = first.Length + 1, m = second.Length + 1;
+            const int delete = 1, insert = 1;
+            var matrix = new int[n, m];
+
+            if (first == null || second == null)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < n; i++) 
+            {
+                matrix[i, 0] = i;
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                matrix[0, i] = i;
+            }
+
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < m; j++)
+                {
+                    var sub = first[i - 1] == second[j - 1] ? 0 : 1;
+
+                    matrix[i, j] = Minimum(matrix[i - 1, j] + delete, matrix[i, j - 1] + insert, matrix[i - 1, j - 1] + sub);
+                    
+                }
+            }
+
+            return matrix[n -1, m -1];
+        }
+        
+    }
+}
---
+++ LearnWord/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using LearnWord.Classes;
+using LearnWord.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Text.Json;
+using Word = Microsoft.Office.Interop.Word;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace LearnWord
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DemoTestBaseEntities db = new DemoTestBaseEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dg.ItemsSource = db.Service.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                WordDocer();
+                //Printer();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        
+        void WordDocer ()
+        {
+            var word = new Word.Application();
+            try
+            {
+                var document = word.Documents.Open(Environment.CurrentDirectory + @"\" + "TEmplate.docx");
+                var table = document.Tables[1];
+
+                var service = db.Service.ToList();
+                int i = 2;
+                foreach (var item in service)
+                {
+                    table.Rows.Add();
+                    table.Cell(i, 1).Range.Text = item.Title;
+                    table.Cell(i, 2).Range.Text = Math.Round(item.Cost, 2).ToString();
+                    table.Cell(i, 3).Range.Text = (item.DurationInSeconds / 60).ToString();
+                    table.Cell(i, 4).Range.Text = (Convert.ToDouble(item.Cost) - (Convert.ToDouble(item.Cost) * Convert.ToDouble(item.Discount)) / 100).ToString();
+                    i++;
+                }
+
+                document.SaveAs2(@"C:\Users\Domovski\Desktop\service.pdf", Word.WdSaveFormat.wdFormatPDF);
+                document.Close(Word.WdSaveOptions.wdDoNotSaveChanges);
+                word.Quit(Word.WdSaveOptions.wdDoNotSaveChanges);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                word.Quit(Word.WdSaveOptions.wdDoNotSaveChanges);
+            }
+        }
+
+
+        void Printer()
+        {
+            PrintDialog printObj = new PrintDialog();
+            if (printObj.ShowDialog() == true)
+            {
+
+                printObj.PrintVisual(dg, "");
+            } else
+            {
+                MessageBox.Show("Вы отменили принт");
+            }
+        }
+
+        
+        
+        private void TxbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var service = db.Service.ToList();
+
+            service = service.Where(x => LevenshteinClass.Distance(TxbSearch.Text.ToLower(), x.Title.ToLower()) <= 3 && 
+                                         LevenshteinClass.Distance(TxbSearch.Text.ToLower(), x.Title.ToLower()) != -1 ||
+                                                          x.Title.ToLower().Contains(TxbSearch.Text.ToLower()) ).ToList();
+
+            dg.ItemsSource = service;
+        }
+
+        private void ToCSV_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream stream = new FileStream(@"C:\Users\Domovski\Desktop\data.csv", FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    var service = db.Service.ToList();
+
+                    //Название заголовков
+                    writer.WriteLine("Название; Длительность; Цена");
+
+                    foreach (var item in service)
+                    {
+                        writer.WriteLine($"{item.Title}; {item.DurationInSeconds}; {item.Cost}");
+                    }
+
+                }
+            }
+
+        }
+
+        private void ToJSON_Click(object sender, RoutedEventArgs e)
+        {
+            var service = db.Service.ToList();
+            var option = new JsonSerializerOptions() { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)};
+            
+            string str = JsonSerializer.Serialize<List<Service>>(service, option);
+
+            File.WriteAllText(@"C:\Users\Domovski\Desktop\filejson.json", str);
+
+        }
+    }
+}
