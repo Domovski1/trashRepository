0d08075173cb13a1843338aa08c50b48a5f8aaf3
Domovski1 <624273242@mail.ru>
2021-03-04 08:19:50 +0300

    хз

---
+++ ConsoleApp1/Classes/XmlConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    class XmlConverter
+    {
+        public void XmlConvert(string Path)
+        {
+            var getData = File.ReadAllLines(Path);
+
+            
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            var getAllFiles = File.ReadAllLines(@"C:\Users\Domovski\Downloads\Telegram Desktop\09_1.5_2\09_1.5_2\Сессия 1\txt1.txt");
+            
+
+            List<Service> listService = new List<Service>();
+
+            foreach (var item in getAllFiles)
+            {
+                var del = item.Split('\t');
+                listService.Add(new Service { 
+                    Title = del[1], 
+                    DurationInSeconds = Convert.ToInt32(del[2]), 
+                    Cost = Convert.ToInt32(del[3]), 
+                    Discount = Convert.ToInt32(del[4])
+                });
+            }
+
+
+            BaseClass.db.Service.AddRange(listService);
+            BaseClass.db.SaveChanges();
+            
+            Console.ReadKey();
+        }
+    }
+}
